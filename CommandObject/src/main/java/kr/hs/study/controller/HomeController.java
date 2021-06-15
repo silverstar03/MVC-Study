e4fb10025017bf2172574159e3ee8e2fdf74c142
silverstar03 <eunbyoul0421@naver.com>
2021-06-15 11:17:44 +0900

    CommandObject 초기 세팅

---
+++ CommandObject/src/main/java/kr/hs/study/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller //이 클래스를 bean으로 등록
+public class HomeController {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	//get방식으로 root로 접속했을 때
+	public String home() {
+		System.out.println("home");
+		return "index";
+	}
+	
+}
