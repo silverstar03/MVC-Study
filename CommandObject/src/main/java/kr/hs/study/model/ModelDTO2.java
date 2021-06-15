1f407b5612b599da90b94cab96a5a7cf045cfb21
silverstar03 <eunbyoul0421@naver.com>
2021-06-15 11:18:49 +0900

    modelDTO 실습2

---
+++ CommandObject/src/main/java/kr/hs/study/model/ModelDTO2.java
@@ -0,0 +1,19 @@
+package kr.hs.study.model;
+
+public class ModelDTO2 {
+	private String uid;
+	private String upass;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}	
+	
+}
