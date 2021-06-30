218a8e3a39532b937d3d881a262a3fe5a93e803d
silverstar03 <eunbyoul0421@naver.com>
2021-06-30 21:19:24 +0900

    main page

---
+++ ViewResolver/src/main/java/kr/hs/study/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println("home");
+		return "index";
+	}
+	
+}
---
+++ ViewResolver/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Hello ViewResolver</h1>
+	<img src="img/logo.svg" width="250" height="100">
+	<jsp:include page="include/menu.jsp"/>
+</body>
+</html>
