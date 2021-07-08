7b02373055421472a76d2b202d9baa236a60737f
silverstar03 <eunbyoul0421@naver.com>
2021-07-08 22:34:12 +0900

    ModelAttribute 사용 실습

---
+++ ViewResolver/src/main/java/kr/hs/study/model/dto/ModelDTO.java
@@ -0,0 +1,26 @@
+package kr.hs.study.model.dto;
+
+public class ModelDTO {
+	private String uid;
+	private String upass;
+	private String uemail;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+	public String getUemail() {
+		return uemail;
+	}
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	
+}
---
+++ ViewResolver/src/main/webapp/WEB-INF/views/test1.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>a: ${param.a }</h1>
+	<h1>b: ${param.b }</h1>
+	<h1>c: ${param.c }</h1>
+</body>
+</html>
